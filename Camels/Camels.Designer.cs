141c9730c12484b08eb5988436458688ec7d81c5
ahinger <andrewjhinger@gmail.com>
2014-01-28 12:27:46 -0500

    firsr

---
+++ Camels/Camels.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Camels
+{
+    partial class Camels
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.waterAmountProgressBar = new System.Windows.Forms.ProgressBar();
+            this.waterAmountNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.waterLabel = new System.Windows.Forms.Label();
+            this.waterAmountLabel = new System.Windows.Forms.Label();
+            this.addWaterButton = new System.Windows.Forms.Button();
+            this.removeWaterButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.waterAmountNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // waterAmountProgressBar
+            // 
+            this.waterAmountProgressBar.Location = new System.Drawing.Point(12, 64);
+            this.waterAmountProgressBar.Name = "waterAmountProgressBar";
+            this.waterAmountProgressBar.Size = new System.Drawing.Size(268, 23);
+            this.waterAmountProgressBar.TabIndex = 0;
+            // 
+            // waterAmountNumericUpDown
+            // 
+            this.waterAmountNumericUpDown.Location = new System.Drawing.Point(158, 38);
+            this.waterAmountNumericUpDown.Name = "waterAmountNumericUpDown";
+            this.waterAmountNumericUpDown.Size = new System.Drawing.Size(59, 20);
+            this.waterAmountNumericUpDown.TabIndex = 1;
+            // 
+            // waterLabel
+            // 
+            this.waterLabel.AutoSize = true;
+            this.waterLabel.Location = new System.Drawing.Point(12, 40);
+            this.waterLabel.Name = "waterLabel";
+            this.waterLabel.Size = new System.Drawing.Size(110, 13);
+            this.waterLabel.TabIndex = 2;
+            this.waterLabel.Text = "Camel Water Amount:";
+            // 
+            // waterAmountLabel
+            // 
+            this.waterAmountLabel.AutoSize = true;
+            this.waterAmountLabel.Location = new System.Drawing.Point(128, 40);
+            this.waterAmountLabel.Name = "waterAmountLabel";
+            this.waterAmountLabel.Size = new System.Drawing.Size(13, 13);
+            this.waterAmountLabel.TabIndex = 3;
+            this.waterAmountLabel.Text = "0";
+            // 
+            // addWaterButton
+            // 
+            this.addWaterButton.Location = new System.Drawing.Point(223, 35);
+            this.addWaterButton.Name = "addWaterButton";
+            this.addWaterButton.Size = new System.Drawing.Size(18, 23);
+            this.addWaterButton.TabIndex = 4;
+            this.addWaterButton.Text = "+";
+            this.addWaterButton.UseVisualStyleBackColor = true;
+            this.addWaterButton.Click += new System.EventHandler(this.addWaterButton_Click);
+            // 
+            // removeWaterButton
+            // 
+            this.removeWaterButton.Location = new System.Drawing.Point(247, 35);
+            this.removeWaterButton.Name = "removeWaterButton";
+            this.removeWaterButton.Size = new System.Drawing.Size(20, 23);
+            this.removeWaterButton.TabIndex = 5;
+            this.removeWaterButton.Text = "-";
+            this.removeWaterButton.UseVisualStyleBackColor = true;
+            this.removeWaterButton.Click += new System.EventHandler(this.removeWaterButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(105, 128);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Camels
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 273);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.removeWaterButton);
+            this.Controls.Add(this.addWaterButton);
+            this.Controls.Add(this.waterAmountLabel);
+            this.Controls.Add(this.waterLabel);
+            this.Controls.Add(this.waterAmountNumericUpDown);
+            this.Controls.Add(this.waterAmountProgressBar);
+            this.Name = "Camels";
+            this.Text = "Camels";
+            this.Load += new System.EventHandler(this.Camels_Load);
+            this.Shown += new System.EventHandler(this.Camels_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.waterAmountNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar waterAmountProgressBar;
+        private System.Windows.Forms.NumericUpDown waterAmountNumericUpDown;
+        private System.Windows.Forms.Label waterLabel;
+        private System.Windows.Forms.Label waterAmountLabel;
+        private System.Windows.Forms.Button addWaterButton;
+        private System.Windows.Forms.Button removeWaterButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Camels/Camels.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Camels
+{
+    public partial class Camels : Form
+    {
+        private Hump _hump = new Hump();
+
+        public Camels()
+        {
+            InitializeComponent();
+        }
+
+        private void addWaterButton_Click(object sender, EventArgs e)
+        {
+            _hump.addWater(Int32.Parse(waterAmountNumericUpDown.Value.ToString()));
+            updateWaterDisplay();
+        }
+
+        private void removeWaterButton_Click(object sender, EventArgs e)
+        {
+            _hump.removeWater(Int32.Parse(waterAmountNumericUpDown.Value.ToString()));
+            updateWaterDisplay();
+        }
+
+        private void updateWaterDisplay()
+        {
+            waterAmountLabel.Text = _hump.GallonsOfWater.ToString();
+            waterAmountProgressBar.Value = _hump.GallonsOfWater;
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Camels_Load(object sender, EventArgs e)
+        {
+            waterAmountProgressBar.Minimum = 0;
+            waterAmountProgressBar.Maximum = _hump.MaximumGallonsOfWater;
+        }
+
+        private void testChangeInteger(ref int value)
+        {
+            value += 10;
+        }
+
+        private void Camels_Shown(object sender, EventArgs e)
+        {
+            int changeInteger = 0;
+
+            testChangeInteger(ref changeInteger);
+            MessageBox.Show("changeInteger value: " + changeInteger);
+        }
+    }
+
+    internal class Hump
+    {
+        private int _gallonsOfWater;
+        private const int _maximumGallonsOfWater = 100;
+
+        internal Hump()
+        {
+            _gallonsOfWater = 0;
+        }
+
+        internal int GallonsOfWater
+        {
+            get { return _gallonsOfWater; }
+        }
+
+        internal int MaximumGallonsOfWater
+        {
+            get { return _maximumGallonsOfWater; }
+        }
+
+        internal void addWater(int gallonsToAdd)
+        {
+            _gallonsOfWater += gallonsToAdd;
+            if (_gallonsOfWater > _maximumGallonsOfWater)
+                _gallonsOfWater = _maximumGallonsOfWater;
+        }
+
+        internal void removeWater(int gallonsToRemove)
+        {
+            _gallonsOfWater -= gallonsToRemove;
+            if (_gallonsOfWater < 0)
+                _gallonsOfWater = 0;
+        }
+    }
+}
